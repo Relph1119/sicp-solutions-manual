b4adac8fa39650c951b49f92095de8501fd1a911
Relph1119 <huruifeng1202@163.com>
2020-11-18 14:39:22 +0800

    完成练习1.18

--- src/practices/ch01/p1-16-fast-expt.scm
+++ src/practices/ch01/p1-16-fast-expt.scm
@@ -6,7 +6,4 @@
 (define (expt-iter b n a)
     (cond ((= n 0) a)
         ((even? n) (expt-iter (square b) (/ n 2) a))
-        (else (expt-iter b (- n 1) (* b a)))))
-
-(define (even? n)
-    (= (remainder n 2) 0))
+        ((odd? n) (expt-iter b (- n 1) (* b a)))))
