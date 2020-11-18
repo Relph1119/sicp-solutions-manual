8045716f29e6744291eea995d74c368fe144a65c
Relph1119 <huruifeng1202@163.com>
2020-11-18 19:30:29 +0800

    完成练习1.19

---
+++ src/practices/ch01/p1-19.scm
@@ -0,0 +1,18 @@
+(load "src/examples/ch01/p8-square.scm")
+
+(define (fib n)
+    (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count) 
+           (fib-iter a 
+                     b 
+                     (+ (square p) (square q)) ;compute p'
+                     (+ (* 2 p q) (square q)) ;compute q'
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
